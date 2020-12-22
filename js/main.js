f0f2d2a5a2dae8a600f1d4cdde101b9399c2d922
hhamza1 <hamzahmitou@gmail.com>
2020-12-22 21:37:15 +0300

    Changing the fill attribute to svg path to change with animation

--- js/main.js
+++ js/main.js
@@ -3,31 +3,35 @@ var socialMedia = document.querySelector(".social-media");
 var authorAvatar = document.querySelector("#author-avatar");
 var articleInfo = document.querySelector(".article-info");
 var shareBtn = document.querySelector(".share-btn");
+var shareIcon = document.getElementsByTagName("path")[0];
+
+
 
 var shareHidden = true;
 
 
 shareBtn.addEventListener("click", function(){
-    if(shareHidden && window.screen.width <= 424){
+    if(shareHidden && window.screen.width <= 1023){
         socialMedia.classList.remove("hide");
         authorAvatar.classList.add("hide");
         articleInfo.classList.add("hide");
         articleDetails.classList.add("dark-bg");
         shareBtn.classList.add("dark-share-btn");
+        shareIcon.setAttribute("fill", "white");
         shareHidden = false;
     }
 
-    else if(shareHidden && window.screen.width >= 425){
+    else if(shareHidden && window.screen.width >= 1024){
         socialMedia.classList.remove("hide");
         shareBtn.classList.add("dark-share-btn");
-        console.log("Share page shown");
+        shareIcon.setAttribute("fill", "white");
         shareHidden = false;
 
     }
-    else if(!shareHidden && window.screen.width >= 425){
+    else if(!shareHidden && window.screen.width >= 1024){
         socialMedia.classList.add("hide");
         shareBtn.classList.remove("dark-share-btn");
-        console.log("Share page shown");
+        shareIcon.setAttribute("fill", "#6E8098");
         shareHidden = true;
 
     }
@@ -37,6 +41,7 @@ shareBtn.addEventListener("click", function(){
         articleInfo.classList.remove("hide");
         articleDetails.classList.remove("dark-bg");
         shareBtn.classList.remove("dark-share-btn");
+        shareIcon.setAttribute("fill", "#6E8098");     
         shareHidden = true;
 
     }
