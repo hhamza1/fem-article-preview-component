cbe38a826d84ca3953570dcec6aa8b72c7049ce2
hhamza1 <hamzahmitou@gmail.com>
2020-12-22 17:19:23 +0300

    Share button element for desktop view

--- js/main.js
+++ js/main.js
@@ -4,14 +4,32 @@ var authorAvatar = document.querySelector("#author-avatar");
 var articleInfo = document.querySelector(".article-info");
 var shareBtn = document.querySelector(".share-btn");
 
+var shareHidden = true;
+
 
 shareBtn.addEventListener("click", function(){
-    if(socialMedia.classList.contains("hide")){
+    if(shareHidden && window.screen.width <= 424){
         socialMedia.classList.remove("hide");
         authorAvatar.classList.add("hide");
         articleInfo.classList.add("hide");
         articleDetails.classList.add("dark-bg");
         shareBtn.classList.add("dark-share-btn");
+        shareHidden = false;
+    }
+
+    else if(shareHidden && window.screen.width >= 425){
+        socialMedia.classList.remove("hide");
+        shareBtn.classList.add("dark-share-btn");
+        console.log("Share page shown");
+        shareHidden = false;
+
+    }
+    else if(!shareHidden && window.screen.width >= 425){
+        socialMedia.classList.add("hide");
+        shareBtn.classList.remove("dark-share-btn");
+        console.log("Share page shown");
+        shareHidden = true;
+
     }
     else {
         socialMedia.classList.add("hide");
@@ -19,5 +37,8 @@ shareBtn.addEventListener("click", function(){
         articleInfo.classList.remove("hide");
         articleDetails.classList.remove("dark-bg");
         shareBtn.classList.remove("dark-share-btn");
+        shareHidden = true;
+
     }
 })
+
